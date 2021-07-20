ab50642049ceaf7d26d29556d42b70d540254e7c
Joran Honig <joran.honig@gmail.com>
2021-07-20 20:41:50 +0200

    update syntax

--- contracts/airDrop.sol
+++ contracts/airDrop.sol
@@ -2,12 +2,12 @@ pragma solidity ^0.6.0;
 
 contract AirDrop {
     
-  constructor() payable {}
+  constructor() public payable {}
 
-  /// #if_succeeds forall i in 1..addresses.length :: addresses[i] != address(0);
-  function sendEth(address[] addresses) public returns (bool) {
+  /// #if_succeeds forall (uint i in 1...addresses.length) addresses[i] != address(0);
+  function sendEth(address payable[] memory addresses) public payable returns (bool) {
       for (uint i = 0; i < addresses.length; i++) {
-          addresses[i].send(0.1);
+          addresses[i].send(1);
       }
   }
 }
